c956c609ec22436592dacec9efcfb61026bf9e90
mrspud1 <32660192+mrspud1@users.noreply.github.com>
2019-02-12 16:04:42 +1300

    Updated, no known errors.

--- calculator.js
+++ calculator.js
@@ -25,6 +25,8 @@ reset();
 function setEventListener(theArray) {
 	for (let i = 0; i < theArray.length; i++) {
   	theArray[i].addEventListener('click', onButtonPress(theArray[i]));
+    theArray[i].addEventListener('mouseover', mouseOver(theArray[i]));
+    theArray[i].addEventListener('mouseout', mouseOut(theArray[i]));
   }
 }
 
@@ -36,9 +38,20 @@ function operate(a,b,operator) {
     : alert("operator not defined");
 }
 
+function mouseOver(button) {
+	return function() {
+  	button.style.backgroundColor = "#BBBBBB";
+  }
+}
+function mouseOut(button) {
+	return function() {
+  	button.style.backgroundColor = "#e2e2e2";
+  }
+}
 
 function onButtonPress(button) {
 	return function() {
+      //button.style.backgroundColor = "#e2e2e2";
       let text = button.textContent;
       if (text === "=") {
       	if (numberArray.length < 1) {
@@ -56,12 +69,19 @@ function onButtonPress(button) {
       	reset();
         return;
       }
+      if (button2.includes(button)) {
+        if (numberArray.length === operatorArray.length) {
+        	return;
+        }
+      }
+      if (displayText.length > 17) {
+      	return;
+      }
       displayText += text;
   		displayLower.textContent = displayText;
       if (button2.includes(button)) { // operators
       	operatorArray.push(text);
         newNumber = true;
-        console.log(numberArray, operatorArray);
       } else if (newNumber) {
 				newNumber = false;
         numberArray[numberArray.length] = text;
@@ -74,9 +94,17 @@ function onButtonPress(button) {
 function calculation(operator) {
 	for (let i = 0; i < operatorArray.length; i++) {
   	if (operatorArray[i] === operator) {
+    	if (operator === "/" && numberArray[i+1] === "0") {
+      	alert("Division by 0!"); 
+        return;
+      }
     	let number1 = parseFloat(numberArray[i]);
       let number2 = parseFloat(numberArray[i+1]);
       let result = operate(number1, number2, operator);
+      if (result.toString().length > 10) {
+        result = result.toString()
+        result = result.substring(0,10); 
+      }
       numberArray.splice(i, 1, result);
       numberArray.splice(i+1, 1);
       operatorArray.splice(i, 1);
